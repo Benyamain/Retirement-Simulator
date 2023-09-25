22973fc5fae8785953b628f41a09d9ee5a969257
Benyamain Yacoob <yacoobbenyamain@gmail.com>
2023-09-24 21:00:37 -0400

    slow and steady wins the race:turtle::snail::smirk::100::thumbsup:

--- src/main/scala/retsim/RetSim.scala
+++ src/main/scala/retsim/RetSim.scala
@@ -2,5 +2,11 @@ package org.retirement.simulator
 package retsim
 
 object RetSim {
-  def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome: Int, currentExpenses: Int, initialCapital: Double) : Double = ???
+  def futureCapital(interestRate: Double, nbOfMonths: Int, netIncome: Int, currentExpenses: Int, initialCapital: Double) : Double = {
+    val monthlySavings = netIncome - currentExpenses
+
+    def nextCapital(accumulated: Double, month: Int) : Double = accumulated * (1 + interestRate) + monthlySavings
+
+    (0 until nbOfMonths).foldLeft(initialCapital)(nextCapital)
+  }
 }
